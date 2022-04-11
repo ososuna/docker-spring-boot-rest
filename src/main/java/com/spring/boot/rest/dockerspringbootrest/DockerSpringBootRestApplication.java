a5dc94d45f6acc17ce6973ef0e1a395592303192
oswi <oswaldo.adrian@live.com.mx>
2022-04-11 17:29:23 -0600

    create spring boot project

---
+++ src/main/java/com/spring/boot/rest/dockerspringbootrest/DockerSpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.rest.dockerspringbootrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringBootRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/rest/dockerspringbootrest/DockerSpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.rest.dockerspringbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
