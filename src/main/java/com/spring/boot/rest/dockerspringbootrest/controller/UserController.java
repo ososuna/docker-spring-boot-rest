579f537ffbd1746f5d954e7b2cca4ae18b22e970
oswi <oswaldo.adrian@live.com.mx>
2022-04-11 21:12:04 -0600

    some details

--- src/main/java/com/spring/boot/rest/dockerspringbootrest/controller/UserController.java
+++ src/main/java/com/spring/boot/rest/dockerspringbootrest/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("user")
 public class UserController {
 
   private final UserService userService;
