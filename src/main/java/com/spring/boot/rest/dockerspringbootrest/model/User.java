8f1ad3725e6c182365affe30a1f42a9e9aed024d
oswi <oswaldo.adrian@live.com.mx>
2022-04-11 21:07:10 -0600

    db config

--- src/main/java/com/spring/boot/rest/dockerspringbootrest/model/User.java
+++ src/main/java/com/spring/boot/rest/dockerspringbootrest/model/User.java
@@ -3,44 +3,26 @@ package com.spring.boot.rest.dockerspringbootrest.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name="User")
-@Table(
-  name = "user",
-  uniqueConstraints = {
-    @UniqueConstraint(
-      name = "email_unique",
-      columnNames = "email"
-    )
-  }
-)
+@Entity()
+@Table(name="users")
 @Getter
 @Setter
 @NoArgsConstructor
 public class User {
   
   @Id
-  @SequenceGenerator(
-    name = "user_sequence",
-    sequenceName = "user_sequence",
-    allocationSize = 1    
-  )
-  @GeneratedValue(
-     strategy = GenerationType.SEQUENCE,
-     generator = "user_sequence"
-  )
+  @GeneratedValue
   @Column(
-    name = "id",
-    updatable = false
+    name="id",
+    nullable=false,
+    unique=true
   )
   private Long id;
   
